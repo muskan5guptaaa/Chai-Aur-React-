4b44427844d903312bb6b1fc141f57ff7c2f19f6
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-05-02 21:23:52 +0530

    Add componenets

--- miniContext06/src/App.jsx
+++ miniContext06/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Profile from './components/Profile'
+import  Login from './components/Login'
+import UserContextProvider from './context/UserContext'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [user, setUser] = React.useState(null)
 
   return (
-    <>
+    <UserContextProvider>
 <h1>Mini Context</h1>
-    </>
+<Login />
+<Profile />
+    </UserContextProvider>
   )
 }
 
