8270088905ac6452011402def2c9e4d220a4d156
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-05-07 21:21:29 +0530

    redux toolkit,blog

---
+++ MegaBlock09/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+function App() {
+console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+     <h1>Muskan</h1>
+    </>
+  )
+}
+
+export default App
---
+++ reduxToolkitTodo08/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Redux</h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
